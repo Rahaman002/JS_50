b712ab3f619a0a301dd89e2a6216aa7533a0175a
Rahaman <36rahaman@gmail.com>
2023-06-30 07:03:32 +0530

    Questions from 1 to 3

---
+++ clone_array.js
@@ -0,0 +1,10 @@
+/*2. Write a JavaScript function to clone an array.
+    Test Data :
+    console.log(array_Clone([1, 2, 4, 0]));
+    console.log(array_Clone([1, 2, [4, 0]]));
+    [1, 2, 4, 0]
+    [1, 2, [4, 0]] 
+*/
+const array_Clone=(arr)=>arr.slice(0);
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
---
+++ first_n_elements.js
@@ -0,0 +1,28 @@
+/*
+3. Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+Test Data :
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+Expected Output :
+7
+[]
+[7, 9, 0]
+[7, 9, 0, -2]
+[]
+ */
+const first=(arr,n)=>{
+    if(n===null) return arr[0];
+    if(arr==[] || n<0)  return [];
+    else
+    {
+        return arr.slice(0,n);
+    }
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
